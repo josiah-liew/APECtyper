bd9d44222f2a482652dee53b9db9960ca70f716d
eam12 <36485497+eam12@users.noreply.github.com>
2022-04-13 13:40:56 -0500

    Degugging

--- APECtyper.sh
+++ APECtyper.sh
@@ -184,7 +184,8 @@ done
 ##### Step 4: Combine Reports (optional) ##### 
 
 COUNT=$(cat ${OUTDIR}/contigFiles.tmp | wc -l)
-[[ "$SUMMARIZE" == 'true' ]] && [[ $COUNT -gt 1 ]] && combineReports && [[ $? -ne 0 ]] && { echo "Error when combining reports in R." ; cleanupOutdir ; exit 1; }
+[[ "$SUMMARIZE" == 'true' ]] && [[ $COUNT -gt 1 ]] && combineReports
+# && [[ $? -ne 0 ]] && { echo "Error when combining reports in R." ; cleanupOutdir ; exit 1; }
         
 #------------------------- Clean-up -------------------------------    
 
