3c1a85dbfc1f1c75406c930b7ecb442eeb769347
eam12 <36485497+eam12@users.noreply.github.com>
2022-04-22 08:30:21 -0500

    $OUTDIR/report.log

--- APECtyper.sh
+++ APECtyper.sh
@@ -72,7 +72,7 @@ function blastAnalysis () {
 
 function generateReport () {
    echo "Generating report..."
-   Rscript "$DIR/bin/outputProcessing.R" "$NAME" "$OUTDIR" "$PERC_COVERAGE" "$PERC_IDENTITY" > report.log 2>&1
+   Rscript "$DIR/bin/outputProcessing.R" "$NAME" "$OUTDIR" "$PERC_COVERAGE" "$PERC_IDENTITY" 2> $OUTDIR/report.log
 }
 
 function compileReports () {
